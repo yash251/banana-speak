fa788ed5d464d0f6929eb5edd32a255f3ec72fb0
Yash Jagtap <jagtapyash2512@gmail.com>
2022-03-26 15:37:24 +0530

    pre final changes

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output"); 
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
@@ -10,7 +9,7 @@ function getTranslationURL(text) {
 }
 
 function errorHandler(error) {
-    console.log("error occurred ", error)
+    console.log("error occured ", error)
     alert("something wrong with server, try again after sometime")
 }
 
