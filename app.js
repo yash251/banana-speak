528fce863879b09870391bdf9ae65890aa29d0ea
Yash Jagtap <jagtapyash2512@gmail.com>
2022-03-26 14:49:58 +0530

    get data from mock server

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output"); 
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
 function clickHandler() {
-    outputDiv.innerText = "yeuyeuyeu " + txtInput.value;
+    // outputDiv.innerText = "yeuyeuyeu " + txtInput.value;
+    var inputText = txtInput.value;
+    fetch(getTranslationURL)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
