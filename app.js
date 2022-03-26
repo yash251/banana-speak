020a5ec1be8a3aa55dd6e35f7949f97f241cfd62
Yash Jagtap <jagtapyash2512@gmail.com>
2022-03-26 15:33:41 +0530

    added minion api from funtranslations api

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output"); 
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
     return serverURL + "?text=" + text;
@@ -10,6 +11,7 @@ function getTranslationURL(text) {
 
 function errorHandler(error) {
     console.log("error occurred ", error)
+    alert("something wrong with server, try again after sometime")
 }
 
 
@@ -19,7 +21,10 @@ function clickHandler() {
 
     fetch(getTranslationURL(inputText)) // calling server for processing
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
     .catch(errorHandler) 
 };
 
